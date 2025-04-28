ada3671b0471885191179dcebc87f312f664c657
HardikMinochaESE <hardik.minocha@colorado.edu>
2025-04-27 21:13:13 -0600

    trying a different way to load the bridge and driver

--- lcd-driver/i2c-lcd-driver-load-unload.sh
+++ lcd-driver/i2c-lcd-driver-load-unload.sh
@@ -7,10 +7,16 @@ case "$1" in
     start)
         echo "Loading i2c-lcd-driver module"
         /sbin/modprobe i2c-lcd-driver
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to load i2c-lcd-driver module"
+            exit 1
+        fi
+        start-stop-daemon -S -n thermal_lcd_bridge -a /etc/init.d/thermal_lcd_bridge.sh 
         ;;
     stop)
         echo "Unloading i2c-lcd-driver module"
         /sbin/rmmod i2c-lcd-driver
+        start-stop-daemon -K -n thermal_lcd_bridge
         ;;
     restart)
         $0 stop
