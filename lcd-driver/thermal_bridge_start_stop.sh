a7fbec0cb6ec6a2b3f5a652cce5965feaa03e243
HardikMinochaESE <hardik.minocha@colorado.edu>
2025-04-27 18:51:29 -0600

    Added start-stop script for thermal_lcd_bridge

---
+++ lcd-driver/thermal_bridge_start_stop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Thermal LCD Bridge daemon start-stop script
+#
+
+case "$1" in
+    start)
+        echo "Starting thermal lcd bridge daemon"
+        start-stop-daemon -S -n thermal_lcd_bridge_daemon -a /usr/bin/thermal_lcd_bridge_daemon.sh
+        ;;
+    stop)
+        echo "Stopping thermal lcd bridge daemon"
+        start-stop-daemon -K -n thermal_lcd_bridge_daemon
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "Usage $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0 
