15029eaa8ff98c898d05ae55337a275eafda626d
HardikMinochaESE <hardik.minocha@colorado.edu>
2025-04-28 12:38:41 -0600

    Removed overlapping initscripts and and merged bdige and lcd driver script into one.

--- lcd-driver/i2c-lcd-driver-load-unload.sh
+++ lcd-driver/i2c-lcd-driver-load-unload.sh
@@ -3,6 +3,9 @@
 # i2c-lcd-driver start-stop-daemon script
 #
 
+PID_FILE="/var/run/thermal_bridge_pid"
+DAEMON_PATH="/etc/init.d/thermal_lcd_bridge_daemon.sh"
+
 case "$1" in
     start)
         echo "Loading i2c-lcd-driver module"
@@ -11,8 +14,17 @@ case "$1" in
             echo "Error: Failed to load i2c-lcd-driver module"
             exit 1
         fi
+        echo "Starting thermal lcd bridge daemon"
+        start-stop-daemon -S -p "$PID_FILE" -m -x "$DAEMON_PATH"
         ;;
     stop)
+        echo "Stopping thermal lcd bridge daemon"
+        if [ -f "$PID_FILE" ]; then
+            PID=$(cat "$PID_FILE")
+            kill "$PID"
+            rm "$PID_FILE"
+        fi
+        echo "Unloading i2c-lcd-driver module"
         /sbin/rmmod i2c-lcd-driver
         ;;
     restart)
